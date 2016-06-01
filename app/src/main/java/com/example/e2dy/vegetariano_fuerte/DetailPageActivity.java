51ce0fd390ba7b1102e481def0947a797a74f248
eddybaez@unicauca.edu.co <you@example.com>
2016-06-01 01:58:07 -0500

    se agrega on click del boton en detail page (sin probarse)

--- app/src/main/java/com/example/e2dy/vegetariano_fuerte/DetailPageActivity.java
+++ app/src/main/java/com/example/e2dy/vegetariano_fuerte/DetailPageActivity.java
@@ -4,18 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.e2dy.vegetariano_fuerte.databinding.ActivityDetailPageBinding;
 import com.example.e2dy.vegetariano_fuerte.databinding.ActivityMainBinding;
+import com.example.e2dy.vegetariano_fuerte.db.RecetaDao;
 import com.example.e2dy.vegetariano_fuerte.models.Receta;
 import com.example.e2dy.vegetariano_fuerte.util.L;
 
-public class DetailPageActivity extends AppCompatActivity{
+public class DetailPageActivity extends AppCompatActivity implements View.OnClickListener{
 
     public static final String EXTRA_POS = "pos";
 
     ActivityDetailPageBinding binding;
+    Receta receta = (Receta) L.data;
 
+
+    RecetaDao dao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,14 +28,14 @@ public class DetailPageActivity extends AppCompatActivity{
         setContentView(binding.getRoot());
 
         int pos = getIntent().getIntExtra(EXTRA_POS,0);
-        Receta receta = (Receta) L.data.get(pos);
+        receta = (Receta) L.data.get(pos);
 
         binding.setReceta(receta);
 
         //setContentView(binding.getRoot());
 
 
-
+        dao = new RecetaDao(this);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Detalle de receta");
@@ -41,5 +46,13 @@ public class DetailPageActivity extends AppCompatActivity{
         if(item.getItemId()== android.R.id.home)
             finish();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        dao.insert(receta);
+
     }
 }
